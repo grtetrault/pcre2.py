7d447cac4b8bdb5077e54077611f6933f2b1ff0e
grtetrault <grtetrault@protonmail.com>
2025-01-13 23:34:15 -0500

    Bumping to 0.5.0

--- src/pcre2/__init__.py
+++ src/pcre2/__init__.py
@@ -12,7 +12,7 @@ from sys import maxsize
 # ============================================================================
 #                                                                    Constants
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __libpcre2_version__ = _cy.__libpcre2_version__
 
 
